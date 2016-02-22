4ddf7ab76583085746044a18319bc422f7b1f954
Jose Miguel <josemiguel.navarro@bbva.com>
2016-02-22 13:02:21 +0100

    added log display

--- index.js
+++ index.js
@@ -189,6 +189,48 @@ app.get('/ramls',function (request, response, next){
         }
     })
 })
+
+function displayLog(filename, request, response, next){
+    var spliter = '******************* ';
+    fs.stat(filename,function(err, stats){
+        if(err){
+            response.end(err);
+        } else if(stats.isFile()){
+            fs.readFile(filename, 'utf8', function(err,data){
+                if(!err){
+                    var splits = data.split('\n');
+                    //var data = [];
+                    //for(var index in splits){
+                    //    //console.log(splits[index])
+                    //    var itemSplits = splits[index].split(',')
+                    //    var item = {}
+                    //    for(var subindex in itemSplits){
+                    //        item[subindex] = itemSplits[subindex]
+                    //    }
+                    //    //console.log(item)
+                    //    data.push(item);
+                    //}
+                    //var headers = data.shift();
+                    //response.render('stats',{headers:headers, data:data})
+                    response.end(splits.join('<br>'))
+                }
+                else
+                    response.end(err);
+            });
+        } else
+            response.end('File '+filename+' is not a file');
+    })
+}
+app.get('/userspro',function (request, response, next){
+    var filename = '/home/ec2-user/workspace/business-intelligence-interfacing/csv/new_users_pro/activated_users.csv';
+    //var filename = 'testQuery.csv'
+    displayLog(filename,request,response,next);
+})
+
+app.get('/usersdev',function (request, response, next){
+    var filename = '/home/ec2-user/workspace/business-intelligence-interfacing/csv/new_users_dev/activated_users.csv';
+    displayLog(filename,request,response,next);
+})
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
